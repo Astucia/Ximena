a6a71d3f67b3013d4545890bddf15d2e250e0e34
alex31016 <alex31016@gmail.com>
2015-01-24 13:45:03 -0600

    Unity Project
    Unity Project

---
+++ Assets/Scripts/FadeOut.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeOut : MonoBehaviour {
+
+	public Image image;
+	private Color color;
+	public float speed;
+	private float fadePercent;
+
+	// Use this for initialization
+	void Start () {
+		color = image.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fadePercent += Time.deltaTime;
+		color.a = Mathf.Lerp(0.0f, 1.0f, fadePercent * speed);
+		image.color = color;
+	}
+}
